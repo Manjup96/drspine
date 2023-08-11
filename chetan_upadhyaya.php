59fc6c8079a14e385a219be8999c45e95f1bc453
Uppala Hemanth <uppalahemanth4@gmail.com>
2023-08-11 15:43:56 +0530

    hemanth

--- chetan_upadhyaya.php
+++ chetan_upadhyaya.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="dr.css">
 
-    <title>Document</title>
-</head>
 
 <?php
 include 'links.php';
